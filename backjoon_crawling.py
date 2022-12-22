e36c6cb411fb07aa3b226e866b8b03b2c3070e26
potential1205 <potential1205@naver.com>
2022-12-22 23:15:08 +0900

    Fix

--- backjoon_crawling.py
+++ backjoon_crawling.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup as bs
 import pickle
 import os
 
-def save_data(data):
+def save_data(data,problem_num):
     if not os.path.exists('data'):
         os.mkdir('data')
 
-    with open('data/test_data.pkl','wb') as f:
+    with open(f'data/{problem_num}.pkl','wb') as f:
         pickle.dump(data,f)
 
 def load_data():
@@ -16,22 +16,21 @@ def load_data():
         data = pickle.load(f)
     print(data)
 
-def crawling(i,submit_num):
-    url_temp =  f"https://www.acmicpc.net/status?&problem_id={i}"
-
-    for i in tqdm(range(submit_num//20)):
+def crawling(problem_num, submit_num):
+    url_temp =  f"https://www.acmicpc.net/status?&problem_id={problem_num}"
+    total_data = []
+    for _ in tqdm(range(submit_num//20)):
         page = requests.get(url_temp)
         soup = bs(page.text, "html.parser")
         elements = soup.select('tbody > tr > td')
         real_time = soup.select('.real-time-update')
-        total_data,single_data = [],[]
-
-        for i in range(len(elements)):
-            if i % 9 == 0:
+        single_data = []
+        for idx,val in enumerate(elements):
+            if idx % 9 == 0:
                 total_data.append(single_data)
                 single_data = []
-                single_data.append(real_time[i//9].attrs['title'])
-            single_data.append(elements[i].get_text())
+                single_data.append(real_time[idx//9].attrs['title'])
+            single_data.append(val.get_text())
 
         try:
             url_temp = "https://www.acmicpc.net" + soup.select('#next_page')[0].attrs['href']
@@ -43,8 +42,8 @@ def crawling(i,submit_num):
 
 if __name__ == "__main__":
     print("hello world")
-    lst = [[1002, 175764]] # 여기에 긁어올 [문제 번호, 제출 수] 넣어주셈
-    for i in range(len(lst)):
-        submit_data = crawling(lst[i][0], lst[i][1])  # crawling(문제번호,긁어올 제출 현황 수)
-        print(submit_data)
-        # save_data(submit_data)
+    lst = [[1002, 200]] # 여기에 긁어올 [문제 번호, 제출 수] 넣어주셈
+    for problem in lst:
+        submit_data = crawling(problem[0], problem[1])  # crawling(문제번호,긁어올 제출 현황 수)
+        print(len(submit_data))
+        save_data(submit_data,problem[0])
